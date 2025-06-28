0f129efcb1be1f97b10b700d50bb582c0bf23f4d
LuskByte <enrique.munguia@m3rsync.com>
2025-06-27 19:31:09 -0600

    (feat) docker listo para prod

---
+++ Dockerfile
@@ -0,0 +1,30 @@
+########################
+# 1. Builder stage
+########################
+FROM node:22.16-alpine AS builder
+
+WORKDIR /home/app
+COPY package*.json ./
+RUN npm ci
+
+COPY tsconfig.json ./
+COPY src ./src
+RUN npx tsc
+
+########################
+# 2. Production stage
+########################
+FROM node:22.16-alpine
+
+WORKDIR /home/app
+ENV NODE_ENV=production
+
+COPY package*.json ./
+RUN npm ci --omit=dev --ignore-scripts
+
+COPY --from=builder home/app/dist ./dist
+COPY docs ./docs 
+COPY scripts/wait-for.sh /usr/local/bin/wait-for
+RUN chmod +x /usr/local/bin/wait-for
+
+CMD ["sh", "-c", "wait-for mysql:3306 -- npx typeorm migration:run -d dist/infraestructure/orm/DataSource.js && node dist/app.js "]
--- src/infraestructure/orm/DataSource.ts
+++ src/infraestructure/orm/DataSource.ts
@@ -1,16 +1,18 @@
 import 'reflect-metadata';
 import { DataSource } from 'typeorm';
-import { User } from '@auth/domain/entities/user.entity';
 
 export const AppDataSource = new DataSource({
   type: 'mysql',
-  host: process.env.DB_HOST || 'localhost',
+  host: process.env.DB_HOST ?? 'localhost',
   port: Number(process.env.DB_PORT ?? 3306),
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
   database: process.env.DB_NAME,
   synchronize: false,
   logging: false,
-  entities: [User],
-  migrations: ['@infra/orm/migrations/*.ts'],
+  entities:
+    process.env.NODE_ENV === 'development'
+      ? ['@auth/domain/entities/*.ts']
+      : ['dist/modules/**/domain/entities/*.js'],
+  migrations: ['dist/migrations/*.js'],
 });
